3e9c3691fcd8cf0842ce2e59cde24bde9f678f71
MalshaL <malsha.13@cse.mrt.ac.lk>
2022-02-25 15:28:14 +1100

    Added access token functionality

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,39 @@ import React from "react";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import LoadingScreen from "./components/LoadingScreen";
+import axios from "axios";
 
 
 export default class App extends React.Component {
-    state = {accessToken: ''};
+
+    constructor(props) {
+        super(props);
+        this.state = {accessToken: ''};
+    }
+
+    getAccessToken = () => {
+        axios.get('http://localhost:5000/api/access-token')
+            .then(response => {
+                this.setState({accessToken: response.data.access_token});
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    componentDidMount() {
+        this.getAccessToken();
+    }
 
     render() {
         return (
             <div className="app">
                 <Header/>
-                {this.state.accessToken ? <HomePage/> : <LoadingScreen/>}
+                {this.state.accessToken ?
+                    <HomePage/> :
+                    <LoadingScreen/>
+                }
             </div>
         )
     }
 }
-
